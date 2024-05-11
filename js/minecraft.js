a6b63717f9fc07d728f14b43b22af30c25a7d420
pablo <pabarragan94@gmail.com>
2024-05-11 15:21:56 +0200

    Deployed 156209b with MkDocs version: 1.5.2

---
+++ js/minecraft.js
@@ -0,0 +1,35 @@
+if (window.location.pathname.includes("minecraft")) {
+    const serverAddress = "pablob.eu";
+    const apiUrl = `https://api.mcstatus.io/v2/status/java/${serverAddress}`;
+    function setServerOnline(isOnline) {
+        serverStatus = isOnline ? "✅ Online." : "🔴 Offline.";
+        document.getElementById("minecraft-status").textContent = serverStatus;
+    }
+    function setServerAddress(address) {
+        document.getElementById("minecraft-address").textContent = address;
+    }
+
+    async function getServerStatus() {
+        try {
+            const response = await fetch(apiUrl);
+            let data = await response.json();
+
+            if (!data) {
+                setServerAddress(serverAddress);
+                return setServerOnline(false);
+            }
+
+            document.getElementById("minecraft-players").textContent =
+                data.players.online;
+            document.getElementById("minecraft-version").textContent =
+                data.version.name_clean;
+            document.getElementById("minecraft-icon").src = data.icon;
+            setServerAddress(data.host);
+            setServerOnline(data.online);
+        } catch (error) {
+            console.error("Error fetching server status:", error);
+            setServerOnline(false);
+        }
+    }
+    getServerStatus();
+}
