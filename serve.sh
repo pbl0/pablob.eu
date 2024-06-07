f0bbdd165d2be8c1e6b0fd7bb0ddf203396f9507
pablo <pabarragan94@gmail.com>
2024-06-08 00:09:10 +0200

    feat: update scripts

--- serve.sh
+++ serve.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+
+./update_mc_world_size.sh;
 .env/bin/python generate_index.py && \
 .env/bin/mkdocs serve;
---
+++ update_mc_world_size.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Get the directory size in GiB with two decimal places
+size=$(ssh pablo@192.168.1.119 'du -sb /config/mcserver/world | awk "{printf \"%.2f GiB\", \$1/1024/1024/1024}"')
+
+echo $size
+
+# Define the file to be edited
+file="/media/datos/Dev/pablob.eu/docs/minecraft.md"
+
+# Escape forward slashes in the size variable for sed
+size_escaped=$(echo "$size" | sed 's/\//\\\//g')
+
+# update file with new world size
+sed -i "s/\(<div id=\"minecraft-world-size\">\).*\(<\/div>\)/\1$size_escaped\2/" "$file"
+
+echo "Updated world map size to ${size} in ${file}"
