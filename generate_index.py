9450c8edad8807abcd9b2db7c85a3b9c3e3f389f
pablo <pabarragan94@gmail.com>
2024-08-12 22:54:51 +0200

    deploy: 2024-08-12

--- generate_index.py
+++ generate_index.py
@@ -56,7 +56,7 @@ def create_index_file(sorted_files, output_file):
         md_file.write("- [My programming projects](code/)\n")
         md_file.write("- [About me](me/)\n")
         md_file.write("- [My Github profile](https://github.com/pbl0)\n")
-        md_file.write("- [My youtube channel](https://www.youtube.com/channel/UCoI4HY4K4CB_J8O_Mphlnbw)\n")
+        md_file.write("- [My youtube channel](https://www.youtube.com/@pbl0_o)\n")
         md_file.write("- [Contact me on Telegram](https://t.me/pablobls)\n")
 
 if __name__ == "__main__":
