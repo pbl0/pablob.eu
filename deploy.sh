75b52572f58cf980002de9ea0c55b67bbb7d2a82
pablo <pabarragan94@gmail.com>
2025-05-30 20:16:23 +0200

    deploy: 2025-05-30

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@
 DATE=$(date --iso-8601);
 git add .
 git commit -m "deploy: $DATE";
-.env/bin/python generate_index.py && \
-.env/bin/mkdocs gh-deploy;
+.venv/bin/python generate_index.py && \
+.venv/bin/mkdocs gh-deploy;
