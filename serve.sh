77ac6b1001cd048e40e4d88d0b7b22332ec0e92b
pablo <pabarragan94@gmail.com>
2026-03-20 17:49:54 +0100

    fix: images width, remove unused css classes

--- serve.sh
+++ serve.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ./update_mc_world_size.sh;
-.venv/bin/python generate_index.py && \
-.venv/bin/mkdocs serve -a 0.0.0.0:8000;
+venv/bin/python generate_index.py && \
+venv/bin/mkdocs serve -a 0.0.0.0:8000;
